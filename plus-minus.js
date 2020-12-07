db603c77957e219754a83480069d23979df8edb4
Shalini Seevagan <shalinimani19@gmail.com>
2020-12-07 15:07:58 +0530

    add plus minus solution

---
+++ plus-minus.js
@@ -0,0 +1,18 @@
+function plusMinus(arr) {
+    let pos = 0;
+    let neg = 0;
+    let zero = 0;
+    arr.forEach(e => {
+        if (e > 0)
+            pos++;
+        else if (e < 0)
+            neg++;
+        else if (e == 0)
+            zero++;
+    });
+    console.log(pos / arr.length);
+    console.log(neg / arr.length);
+    console.log(zero / arr.length);
+}
+var result=plusMinus([-3,0,-6,3,-1,2]);
+console.log(result);
