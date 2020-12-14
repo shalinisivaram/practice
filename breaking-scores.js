630229220b179f28ce2162c2dae716e250a0fdd3
Shalini Seevagan <shalinimani19@gmail.com>
2020-12-14 16:49:12 +0530

    add breaking scores solution

---
+++ breaking-scores.js
@@ -0,0 +1,15 @@
+function breakingRecords(scores) {
+    let [ hi, lo ] = [ scores[0], scores[0] ];
+    let [ max, min ] = [ 0, 0 ];
+    
+    // Calculating
+    for (let i = 1; i < scores.length; i++) {
+        if (scores[i] > hi) { hi = scores[i]; max++; }
+        if (scores[i] < lo) { lo = scores[i]; min++; }
+    }
+    
+    //Returning
+    return [ max, min ];
+    }
+    var result=breakingRecords([73,67,38,33]);
+console.log(result);
